fc04b8c662f0b61a6a1005c7a44a7bff18746555
Vincent M. van der Meulen <meulen@uw.edu>
2017-07-02 03:03:33 -0700

    Final changes

--- src/components/Footer/Wrapper.js
+++ src/components/Footer/Wrapper.js
@@ -2,8 +2,6 @@ import styled from 'styled-components';
 
 const Wrapper = styled.footer`
     background-color: #7c4b35;
-    position: absolute;
-    bottom: 0;
     width: 100%;
     height: 64px;
 `;
